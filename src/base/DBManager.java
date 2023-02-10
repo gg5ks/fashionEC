71531900c5db4dfcbc219800fc6e5be831d96956
kobayashi-s <kobayashi-s@groove-gear.co.jp>
2023-02-10 16:27:34 +0900

    <chore>:<#11 ><DBManagerディレクトリの作成>

---
+++ src/base/DBManager.java
@@ -0,0 +1,29 @@
+package base;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBManager {
+    public static Connection getConnection(){
+
+        Connection con = null;
+
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/fashionEC_db?useUnicode=true&characterEncoding=UTF8","root","");
+            System.out.println("DBConnected!!");
+            return con;
+
+        }catch(ClassNotFoundException e){
+        	System.out.println("ClassNotFound");
+            throw new IllegalMonitorStateException();
+        }catch(SQLException e){
+        	System.out.println("SQLException");
+            throw new IllegalMonitorStateException();
+        }catch(Exception e){
+        	System.out.println("Exception");
+            throw new IllegalMonitorStateException();
+        }
+    }
+}
