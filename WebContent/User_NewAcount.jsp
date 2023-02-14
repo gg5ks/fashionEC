f883ff3497cf1d1ce9a9e99010c55a3b4b4a2193
kobayashi-s <kobayashi-s@groove-gear.co.jp>
2023-02-14 11:41:52 +0900

    <feat>:<#22 ><新規会員登録機能の実装完了>

--- WebContent/User_NewAcount.jsp
+++ WebContent/User_NewAcount.jsp
@@ -9,17 +9,17 @@
 <body>
 	<form action="User_NewAcountRegi">
 		ユーザー名
-		<input type="text" name="name">
+		<input type="text" name="name" required>
 		Email
-		<input type="text" name="mail">
+		<input type="text" name="mail" required>
 		パスワード
-		<input type="text" name="pass">
+		<input type="text" name="pass" required>
 		郵便番号
-		<input type="number" name="post">
+		<input type="number" name="post" required>
 		住所
-		<input type="text" name="address">
+		<input type="text" name="address" required>
 		電話番号
-		<input type="text" name="tell">
+		<input type="text" name="tell" required>
 		<input type="submit" value="新規会員登録">
 	</form>
 </body>
--- src/jums/User_NewAcountRegi.java
+++ src/jums/User_NewAcountRegi.java
@@ -7,6 +7,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class User_NewAcountRegi
@@ -28,8 +29,41 @@ public class User_NewAcountRegi extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		request.getRequestDispatcher("/User_TopPage.jsp").forward(request, response);
-	}
+		request.setCharacterEncoding("UTF-8");
+		HttpSession session = request.getSession();
+		try{
+			//form値の受け取り
+			String name = request.getParameter("name");
+			String mail = request.getParameter("mail");
+			String pass = request.getParameter("pass");
+			Integer post = Integer.parseInt(request.getParameter("post"));
+			String address = request.getParameter("address");
+			String tell = request.getParameter("tell");
+			
+            //DTOオブジェクトにマッピング。DB専用のパラメータに変換
+            UserDataDTO udd = new UserDataDTO();
+            udd.setUser_name(name);
+            udd.setUser_email(mail);
+            udd.setUser_password(mail);
+            udd.setUser_postid(post);
+            udd.setUser_address(address);
+            udd.setUser_tell(tell);
+
+            //DBへデータの挿入
+            UserDataDAO .getInstance().NewUser(udd);
+
+            //結果画面での表示用に入力パラメータ―をリクエストパラメータとして保持
+            //request.setAttribute("AcCompUDB", AcCompUDB);
+
+            request.getRequestDispatcher("/User_TopPage.jsp").forward(request, response);
+        }catch(Exception e){
+            //何らかの理由で失敗したらエラーページにエラー文を渡して表示。想定は不正なアクセスとDBエラー
+        	e.printStackTrace();
+			request.setAttribute("error", e.toString());
+            request.getRequestDispatcher("/error.jsp").forward(request, response);
+        }
+}
+		
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
